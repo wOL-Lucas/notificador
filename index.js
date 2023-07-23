5a3e3bc108fc43028cf434829020768eb3232cfa
Lucas Eduardo Fernandes <lucasefernandes333@gmail.com>
2023-07-23 20:04:53 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+"use strict";
+
+function __init__(){
+
+    console.log('inited')
+
+    const element = document.getElementsByClassName('lnXdpd')
+    console.log(element)
+}
+
+console.log('13901-231209832019')
+__init__();
